722b7c8d8be5942616db18647d7f13711aaddfd6
Swayam Bhoir <swayambhoir579@gmail.com>
2024-09-01 15:28:43 +0530

    Add User Authentication

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -24,6 +24,7 @@ function Navbar() {
         sidebar.current.classList.toggle("open")
     }
 
+
     const handleLogout = () => {
         signOut(auth)
             .then(() => {
--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIbh81lRDmHtEFnJ5M_nOnLZpyiCV9JQo",
-    authDomain: "fir-project-212de.firebaseapp.com",
-    projectId: "fir-project-212de",
-    storageBucket: "fir-project-212de.appspot.com",
-    messagingSenderId: "474483075411",
-    appId: "1:474483075411:web:56b15f585792d79e17d9a6",
-    measurementId: "G-6G6864TEQP"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
 }
 
 export const app = initializeApp(firebaseConfig)
